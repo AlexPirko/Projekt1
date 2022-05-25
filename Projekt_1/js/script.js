b62dfc1f1f3b8d03a1279af504f9963cb057f67f
Alex <youzhny.foto@gmail.com>
2022-05-25 10:50:36 +0300

    part_3-done!

--- Projekt_1/js/script.js
+++ Projekt_1/js/script.js
@@ -1,6 +1,55 @@
+// 'use strict';
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log("Вы классический зритель");
+// } else if (personalMovieDB.count >= 30) {
+//     console.log("Вы киноман");
+// } else {
+//     console.log("Произошла ошибка");
+// }
+
+// console.log(personalMovieDB);
+
+
+
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,27 +59,45 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
+rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+function detectPerconalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
 }
+detectPerconalLevel();
 
-console.log(personalMovieDB);
+function showMyDB() {
+    if (!personalMovieDB.privat) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}?`, '');     
+    }
+}
+writeYourGenres();
--- Test JS/js/scripts.js
+++ Test JS/js/scripts.js
@@ -3,3 +3,169 @@ let newArr = arr.map(function (item) {
     return item * 2;
 });
 console.log(newArr);
+
+function showName() {
+    console.log('Vasya');
+}
+setTimeout(showName, 0);
+console.log('Kolya');
+
+
+showMessage();
+function showMessage() {
+    console.log('Message!')
+}
+
+showMessage();
+let showMessage = function () {
+    console.log('Message!')
+}
+
+'use strict'
+
+if (2 > 1) {
+    function showMessage() {
+        console.log('Message!');
+    }
+}
+showMessage();
+
+let num = 1;
+while (num <= 5) {
+    console.log(num);
+    num++;
+}
+
+let num = 1;
+do {
+    console.log(num);
+    num++;
+}   while (num < 6);
+
+for (let num=1; num<6; num++) {
+    console.log(num);
+}
+
+let num = 8;
+while (num) {
+    console.log(num);
+    num--;
+}
+
+let num = 0;
+while (num<3) {
+    console.log(`Число: ${num}`);
+    num++;
+}
+
+
+for (let num = 0; num<2; num++) {
+    if(num == 1) break;
+    for (let size = 0; size < 3; size++) {
+        console.log(size);
+    }
+}
+
+let userInfo = {
+    name: "Vasya",
+    age: 30,
+    adress: {
+        city: "Odessa",
+        street: "Ode"
+    }
+}
+
+for (let key in userInfo.adress) {
+   // console.log(key);
+    console.log(userInfo.adress[key]);
+}
+
+let arr = ['Ваня', 'Иштван', 'Оля',];
+let newArr = arr;
+newArr.push('Петя');
+console.log(arr);
+
+
+let users = ['Ваня', 'Иштван'];
+users.push('Оля');
+users[1] = 'Петя';
+console.log(users);
+let removed = users.splice (1, 1);
+console.log(removed);
+users.unshift('Маша', 'Паша');
+console.log(users);
+
+
+let arr = ['Ваня', 'Иштван', 'Оля',];
+let removed = arr.splice (1, 1);
+console.log(removed);
+
+
+let str = 'Ваня, Иштван, Оля';
+let arr = str.split(', ');
+console.log(arr);
+
+
+let arr = [9, 2, 8,];
+let reduceValue = arr.reduce (function (previousValue, item, index, array) {
+return item + previousValue;
+}, 0);
+
+console.log(reduceValue);
+
+
+let fls = 'фрилансер';
+let text = `Привет! Я ${fls}`;
+console.log(text);
+
+
+let text = 'фрилансер';
+//console.log(text.slice(5, 6));
+//console.log(text.toUpperCase());
+console.log(text.includes('лан', 4));
+
+
+let text = 123 + +"456";
+console.log(text);
+
+
+let numOne = Math.round((1.005 + Number.EPSILON)*100) / 100;
+console.log(numOne);
+
+
+let value = 58 + "Фрилансер";
+if (isNaN(value)) {
+    console.log('Результат выражения NaN');
+}
+
+
+let value = "135.58px";
+let newValue = parseFloat("135.58px");
+console.log(newValue);
+
+
+console.log(Math.max(10, 58, 39, -150, 0));
+
+console.log(Math.floor(58.858));
+
+
+const userInfo = {
+    name: "Vasya",
+    age: 30,
+    "likes js": true
+    }
+
+    console.log(userInfo["likes js"]);
+
+
+
+let userInfo = {
+    name: "Vasya",
+}
+userInfo.age = 30;
+
+userInfo.name = "Lena";
+
+delete userInfo.name;
+
+console.log(userInfo);
